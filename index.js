1e723add3f7cf2925b510f5b0e659f438dbdfb82
mohammad badangiz <mbz3a1998@gmail.com>
2021-09-15 03:11:12 +0430

    after add express

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {Telegraf} = require("telegraf");
-bot = new Telegraf('1960602083:AAE_Gf8uXYD9xzrQtZZMcwAV6RV0gUqVBtE');
+const {Express} = require("express");
+
+const bot = new Telegraf('1960602083:AAE_Gf8uXYD9xzrQtZZMcwAV6RV0gUqVBtE');
+const ex = new Express();
 
 bot.start(response => response.reply('sss'));
 bot.launch();
